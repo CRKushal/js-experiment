c35f2f06f31b8879b08e35b6736ac16bc96d8691
kushal <crkushal7@gmail.com>
2018-12-03 12:13:27 +0545

    Final ball collison

---
+++ ballCollison/script.js
@@ -0,0 +1,114 @@
+var ballsArray = [];
+
+var randomGenerator = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function Game(ballsNumber, width, height) {
+    this.ballsNumber = ballsNumber;
+    this.width = width;
+    this.height = height;
+    var boundary = {
+        width: this.width,
+        height: this.height
+    };
+
+    var box = document.getElementById('box');
+    box.style.width = boundary.width + 'px';
+    box.style.height = boundary.height + 'px';
+    box.style.backgroundColor = '#ccc';
+    box.style.position = 'realative';
+
+    this.init = function () {
+        for (var i = 0; i < this.ballsNumber; i++) {
+            var circle = {
+                x: randomGenerator(25, boundary.width - 20),
+                y: randomGenerator(25, boundary.height - 20),
+                radius: randomGenerator(8, 20),
+                color: "rgb(" + randomGenerator(20, 256) + "," + randomGenerator(20, 256) + "," + randomGenerator(20, 256) + ")"
+            }
+
+            var player = new Ball(circle, boundary, box);
+            player.drawBall();
+
+            ballsArray.push(player);
+        }
+        setInterval(this.startGame, 10);
+    }
+
+    this.startGame = function () {
+        for (var i = 0; i < ballsArray.length; i++) {
+            ballsArray[i].moveBall();
+            ballsArray[i].collison();
+        }
+    }
+}
+var game = new Game(20, 600, 600).init();
+
+function Ball(circle, boundary, box) {
+    this.x = circle.x;
+    this.y = circle.y;
+    this.width = circle.radius * 2;
+    this.height = circle.radius * 2;
+    this.box = box;
+    this.boundary = boundary;
+    this.col = circle.color;
+    this.radius = circle.radius;
+
+    this.speed = 1;
+    this.vel = 1;
+    var dir = [-1, 1];
+    this.Xdirection = dir[randomGenerator(0, 1)];
+    this.Ydirection = dir[randomGenerator(0, 1)];
+
+    var ball = document.createElement('div');
+
+    this.drawBall = function () {
+        this.box.appendChild(ball);
+        ball.style.width = this.width + 'px';
+        ball.style.height = this.height + 'px';
+        ball.style.borderRadius = '50%';
+        ball.style.backgroundColor = this.col;
+        ball.style.position = 'absolute';
+        ball.style.left = (this.x - this.radius) + 'px';
+        ball.style.top = (this.y - this.radius) + 'px';
+    }
+
+
+    this.moveBall = function () {
+        ball.style.left = this.x + 'px';
+        ball.style.top = this.y + 'px';
+
+        this.y += this.Ydirection * this.speed;
+        this.x += this.Xdirection;
+
+        if (this.x + this.height < 20 + this.height || this.x + this.height > boundary.height) {
+            this.Xdirection = -this.Xdirection;
+
+        } else if (this.y + this.width < 20 + this.width || this.y + this.width > boundary.width) {
+            this.Ydirection = -this.Ydirection;
+        }
+    }
+
+    this.collison = function () {
+        for (var i = 0; i < ballsArray.length; i++) {
+            if (this == ballsArray[i]) {
+                continue;
+            }
+            var other = ballsArray[i];
+            var distance = this.distance(other.x, other.y);
+            if (this.radius + other.radius > distance) {
+                this.Xdirection = -this.Xdirection;
+                this.Ydirection = -this.Ydirection;
+                other.Xdirection = -other.Xdirection;
+                other.Ydirection = -other.Ydirection;
+            }
+        }
+    }
+    this.distance = function (otherX, otherY) {
+        var dX = otherX - this.x;
+        var dY = otherY - this.y;
+        var dist = Math.sqrt((dX * dX) + (dY * dY));
+        return dist;
+    }
+}
